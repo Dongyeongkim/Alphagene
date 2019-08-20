ef2b91ccd3dfec31815ee59367542f737cf45b53
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2019-08-20 22:59:41 +0900

    Update Model_Converter.py
    Finally Fixed the Last Bug

--- GDQN/Model_Converter.py
+++ GDQN/Model_Converter.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class GeneticModel:
     def __init__(self,Mgen,Sgen,optim=tf.keras.optimizers.Adam,lr=1e-7):
         self.lr = lr; self.optim = optim(learning_rate=lr)
@@ -28,61 +29,56 @@ class GeneticModel:
     def Calc_UpSampling_HP(self, PS_gen1, PS_gen2):
         size = int(PS_gen2/PS_gen1)
         return size
-
+    
     def Convert2Model(self,Mgen,Sgen):
         model = tf.keras.models.Sequential()
         Master_Gen = (lambda x: [x[3 * i:3 * i + 3] for i in range(11)])(Mgen)
         Sgen = (lambda x: [x[2 * i:2 * i + 2] for i in range(11)])(Sgen)
         Valid_Sgen_List = []; Valid_Sgen_List.append('54')
-
         Valid_Mgen_List = []
-        
+       
         for i in range(len(Master_Gen)):
             if((Master_Gen[i]!='011')and(Master_Gen[i]!='110')):
                 Valid_Mgen_List.append(Master_Gen[i])
                 Valid_Sgen_List.append(Sgen[i])
             if(Master_Gen[i]=='111'):
                 break
-        print(Valid_Mgen_List,Valid_Sgen_List)
+       
         for i in range(len(Valid_Mgen_List)):
+            Prev = 1
             try:
-                Prev = int(Valid_Sgen_List[i],16)
-                Post = int(Valid_Sgen_List[i+1],16)
+                Prev = model.layers[i].output_shape[1]
+                Post = int(Valid_Sgen_List[i],16)
+                
+                
             except IndexError:
-                Prev = int(Valid_Sgen_List[i],16)
                 Post = 4
             if(i==0):
                 if (Prev >= Post):
                     if (Valid_Mgen_List[i] == '010'):
-                        print('Hi')
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2,activation='relu',input_shape=(84,84,4)))
 
                     elif (Valid_Mgen_List[i] == '111'):
-                        print('Hi')
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2, activation='softmax',input_shape=(84,84,4)))
                         break
                     elif (Valid_Mgen_List[i] == '000'):
-                        print('Hi')
                         Kernel_Size = self.Calc_Conv_HP(Prev, Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='relu',input_shape=(84,84,4)))
                    
                     elif (Valid_Mgen_List[i] == '001'):
-                        print('Hi')
                         Strides, pooling_size = self.Calc_Pool_HP(Prev, Post)
                         model.add(tf.keras.layers.AveragePooling2D(pool_size=(pooling_size,pooling_size),
                         strides=(Strides,Strides),input_shape=(84,84,4)))
                     
                     elif (Valid_Mgen_List[i] == '100'):
-                        print('Hi')
                         Kernel_Size = self.Calc_Conv_HP(Prev, Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='sigmoid',input_shape=(84,84,4)))
                   
                     elif (Valid_Mgen_List[i] == '101'):
-                        print('Hi')
                         Kernel_Size = self.Calc_Conv_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='relu',input_shape=(84,84,4)))
@@ -90,34 +86,28 @@ class GeneticModel:
 
                 else:
                     if (Valid_Mgen_List[i] == '010'):
-                        print('Hi')
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2,activation='relu',input_shape=(84,84,4)))
                  
                     elif (Valid_Mgen_List[i] == '111'):
-                        print('Hi')
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2,activation='softmax',input_shape=(84,84,4)))
                         break
                     elif (Valid_Mgen_List[i] == '000'):
-                        print('Hi')
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='relu',input_shape=(84,84,4)))
                 
                     elif (Valid_Mgen_List[i] == '001'):
-                        print('Hi')
                         Size = self.Calc_UpSampling_HP(Prev,Post)
                         model.add(tf.keras.layers.UpSampling2D(size=(Size,Size),input_shape=(84,84,4)))
                    
                     elif (Valid_Mgen_List[i] == '100'):
-                        print('Hi')
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='sigmoid',input_shape=(84,84,4)))
                        
                     elif (Valid_Mgen_List[i] == '101'):
-                        print('Hi')
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='relu',input_shape=(84,84,4)))
@@ -130,64 +120,81 @@ class GeneticModel:
                         # Sgen = Prev
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2, activation='relu'))
+                        
                     elif (Valid_Mgen_List[i] == '111'):
                         # Sgen = Prev
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2, activation='softmax'))
+                        
                         break
                     elif (Valid_Mgen_List[i] == '000'):
                         Kernel_Size = self.Calc_Conv_HP(Prev, Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='relu'))
+                       
                     elif (Valid_Mgen_List[i] == '001'):
                         Strides, pooling_size = self.Calc_Pool_HP(Prev, Post)
                         model.add(tf.keras.layers.AveragePooling2D(pool_size=(pooling_size,pooling_size),
                         strides=(Strides,Strides)))
+                        
                     elif (Valid_Mgen_List[i] == '100'):
                         Kernel_Size = self.Calc_Conv_HP(Prev, Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='sigmoid'))
+                        
                     elif (Valid_Mgen_List[i] == '101'):
-                        model.add(tf.keras.layers.GaussianNoise(1))
                         Kernel_Size = self.Calc_Conv_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2D(16,kernel_size=(Kernel_Size, Kernel_Size),
                         activation='relu'))
+                        model.add(tf.keras.layers.GaussianNoise(1))
+                        
 
                 else:
                     if (Valid_Mgen_List[i] == '010'):
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2, activation='relu'))
+                    
                     elif (Valid_Mgen_List[i] == '111'):
                         t = Prev
                         model.add(tf.keras.layers.Dense(t ** 2, activation='softmax'))
+                    
                         break
                     elif (Valid_Mgen_List[i] == '000'):
+                        
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='relu'))
+                        
                     elif (Valid_Mgen_List[i] == '001'):
+                        
                         Size = self.Calc_UpSampling_HP(Prev,Post)
                         model.add(tf.keras.layers.UpSampling2D(size=(Size,Size)))
+                        
                     elif (Valid_Mgen_List[i] == '100'):
+                        
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='sigmoid'))
+                        
                     elif (Valid_Mgen_List[i] == '101'):
-                        model.add(tf.keras.layers.GaussianNoise(1))
+                        
                         kernel_size = self.Calc_ConvTranspose_HP(Prev,Post)
                         model.add(tf.keras.layers.Conv2DTranspose(16,kernel_size=(kernel_size,kernel_size),
                         activation='relu'))
+                        model.add(tf.keras.layers.GaussianNoise(1))
+                        
+             
 
         model.add(tf.keras.layers.Flatten());model.summary()
         model.compile(
             loss=lambda targ, pred: tf.compat.v1.losses.huber_loss(labels=targ, predictions=pred),
             metrics=['accuracy', 'mae'], optimizer=self.optim)
-        Mgen = ''.join(Mgen); model.save('model/'+Mgen+'.h5')
+        
+        model.save('model/'+Mgen+'.h5')
         print('MODEL Constructing has DONE')
 
         return model
-
-
+    
     def optimizer(self):
         action = tf.placeholder('int32', (None,))
         target = tf.placeholder('float32', (None,))
